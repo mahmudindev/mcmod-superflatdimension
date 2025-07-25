f41dfc120e881ef399858f52e5403258a117be81
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-07-25 14:09:43 +0700

    Updates to 1.21

---
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/superflatdimension/neoforge/SuperflatDimensionNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.mahmudindev.mcmod.superflatdimension.neoforge;
+
+import com.github.mahmudindev.mcmod.superflatdimension.SuperflatDimension;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SuperflatDimension.MOD_ID)
+public final class SuperflatDimensionNeoForge {
+    public SuperflatDimensionNeoForge() {
+        // Run our common setup.
+        SuperflatDimension.init();
+    }
+}
